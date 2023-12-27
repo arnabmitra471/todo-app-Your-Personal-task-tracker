791c6953042f90b7f8df9da29600a32e7a5a7ae1
Arnab Mitra <arnabmitra471@gmail.com>
2023-12-27 08:44:41 +0530

    Now using mysql as our database engine

--- todolist/settings.py
+++ todolist/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "home",
 ]
 
 MIDDLEWARE = [
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'todolist.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': "todolist_userData",
+        'USER': "root",
+        'PASSWORD': "",
+        "HOST": "localhost",
+        "PORT": "3306",
     }
 }
 
