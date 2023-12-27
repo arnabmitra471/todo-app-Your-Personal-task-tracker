6ffa829ab64537827ed73cf92698e3f5402852b3
Arnab Mitra <arnabmitra471@gmail.com>
2023-12-27 10:32:30 +0530

    Storing the data entered in the form to the database

--- home/models.py
+++ home/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Tasks(models.Model):
+    taskTitle = models.CharField(max_length=30)
+    taskDesc = models.TextField(default=None)
+    taskTime = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.taskTitle
--- home/views.py
+++ home/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from .models import Tasks
 # Create your views here.
 
 def home(request):
+    if request.method == "POST":
+        taskTitle = request.POST["title"]
+        taskDesc = request.POST["description"]
+        taskDetails = Tasks(taskTitle=taskTitle,taskDesc=taskDesc)
+        taskDetails.save()
+        print("The data has been written to the database successfully")
     return render(request,"index.html")
 
 def show_tasks(request):
